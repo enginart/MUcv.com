66fea5b83672f2fe913d13455097ad49f8ac75f3
tzou2024 <tzou@olin.edu>
2023-08-02 13:09:19 -0400

    rename songfiles path

--- src/pages/BrowseMashups.js
+++ src/pages/BrowseMashups.js
@@ -9,20 +9,20 @@ export const BrowseMashups = () => {
     const shuffled = data.sort(() => 0.5 - Math.random());
 
     // Get sub-array of first n elements after shuffled
-    let selected = shuffled.slice(0,7);
+    let selected = shuffled.slice(0,10);
     
 
 
     return (
         <div className='bod'>
             <div className='body-box'>
-                <div className='about-title'>
+                <div className='browse-mashup-title'>
                 Listen to a Mashup
                 </div>
                 <div className='view-mashup-list'>
                 {selected.map((val, ind) => 
                 
-                <Button href={`/MUcv.com/ViewMashups/${val.muName}`} className='home-button'>
+                <Button href={`/MUcv.com/ViewMashups/${val.muName}`} className='view-mashup-button'>
                     {`${val.sAatitle}`} + { `${val.sBatitle}`}
                 </Button>
                 )}
