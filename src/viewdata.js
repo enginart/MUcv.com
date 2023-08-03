c5c0c6ec3c1f3892d421c949ee0594210e6333f9
tzou2024 <tzou@olin.edu>
2023-08-02 23:15:40 -0400

    fix height on view mashups

---
+++ src/viewdata.js
@@ -0,0 +1,42 @@
+listy = [
+    {
+        'sA': "Bleeding Love",
+        'sB': "Just the Way You Are"
+    },
+    {
+        'sA': "Cheap Thrills",
+        'sB': "Talk Dirty",
+    },
+    {
+        'sA': "Evacuate the Dance Floor",
+        'sB': "Set Fire to the Rain",
+    },
+    {
+        'sA': "Gangnam Style",
+        'sB': "Diamonds",
+    },
+    {
+        'sA': "Happy",
+        'sB': "E.T.",
+    },
+    {
+        'sA': "Kiss Kiss",
+        'sB': "Hollaback Girl",
+    },
+    {
+        'sA': "Let Me Lover You",
+        'sB': "Worth it",
+    },
+    {
+        'sA': "Love Lockdown",
+        'sB': "Because",
+    },
+    {
+        'sA': "Rockabye",
+        'sB': "Chained to the Rhythms",
+    },
+    {
+        'sA': "Stressed Out",
+        'sB': "Heathens",
+    },
+]
